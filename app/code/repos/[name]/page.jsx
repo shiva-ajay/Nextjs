300cce8f5c8a54bf03eec70c86359ed0baac501b
Shiva Ajay Veeragani <shivaajay987@gmail.com>
2024-08-26 19:00:46 +0530

    Final

--- app/code/repos/[name]/page.jsx
+++ app/code/repos/[name]/page.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import { Suspense } from 'react';
+import Link from 'next/link';
+import Repo from '@/app/components/Repo';
+import RepoDirs from '@/app/components/RepoDirs';
 
-const page = () => {
+const RepoPage = ({ params: { name } }) => {
   return (
-    <div>page</div>
-  )
-}
-
-export default page
+    <div className='card'>
+      <Link href='/code/repos' className='btn btn-back'>
+        Back To Repositories
+      </Link>
+      <Suspense fallback={<div>Loading repo...</div>}>
+        <Repo name={name} />
+      </Suspense>
+      <Suspense fallback={<div>Loading directories...</div>}>
+        <RepoDirs name={name} />
+      </Suspense>
+    </div>
+  );
+};
+export default RepoPage;
